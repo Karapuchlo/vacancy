f51193bd0274ecc62b6b0659f4063b1cd76a7604
grayd <sharin@you-x.ru>
2024-08-18 14:51:04 +0300

    добавил обстрактный класс

--- src/unyts.py
+++ src/unyts.py
@@ -1,12 +1,44 @@
-# parser
 import requests
-import csv
 import json
-from src.vacancy import Vacancy
+from abc import ABC, abstractmethod
 
-class HHAPIParser:
-    def fetch_vacancies_hh(self):
-    # Задаем параметры запроса
+class Vacancy:
+    def __init__(self, employer_name, name, salary, alternate_url):
+        self.employer_name = employer_name
+        self.name = name
+        self.salary = salary
+        self.alternate_url = alternate_url
+
+    def __str__(self):
+        return f"Вакансия {self.name} в {self.employer_name} с зарплатой {self.salary}"
+
+    def __repr__(self):
+        return f"Vacancy(employer_name='{self.employer_name}', name='{self.name}', salary='{self.salary}', alternate_url='{self.alternate_url}')"
+
+    @property
+    def dict(self):
+        return {
+            'employer_name': self.employer_name,
+            'name': self.name,
+            'salary': self.salary,
+            'alternate_url': self.alternate_url
+        }
+
+class VacancyParser(ABC):
+    @abstractmethod
+    def fetch_vacancies(self):
+        """Получить список вакансий"""
+        pass
+
+    @abstractmethod
+    def export_vacancies(self, vacancies, filename):
+        """Экспортировать список вакансий в файл"""
+        pass
+
+class HHAPIParser(VacancyParser):
+    def fetch_vacancies(self):
+        # Задаем параметры запроса
+        vacancies = []
         for i in range(10):
             params = {
                 'text': 'Python разработчик',
@@ -27,37 +59,52 @@ class HHAPIParser:
             js_hh = json.loads(data)
 
             # Обрабатываем полученные данные
-            vacancies = []
             for vacancy_data in response.json()['items']:
                 if vacancy_data['salary'] is not None:
-                    vacancy = Vacancy(vacancy_data["name"], vacancy_data["employer"]["name"], vacancy_data['salary'], vacancy_data["alternate_url"])
+                    vacancy = Vacancy(
+                        vacancy_data["name"],
+                        vacancy_data["employer"]["name"],
+                        vacancy_data['salary'],
+                        vacancy_data["alternate_url"]
+                    )
                     vacancies.append(vacancy)
 
-            # Возвращаем список вакансий
-            return vacancies
+        # Возвращаем список вакансий
+        return vacancies
 
-
-# exporter
-    def export_vacancies_hh(self, vacancies):
+    def export_vacancies(self, vacancies, filename='vacancies.json'):
         # Экспортируем вакансии в json-файл
-        with open('vacancies.json', 'w', encoding="UTF-8") as jsonfile:
-            json.dump([v.__dict__ for v in vacancies], jsonfile, indent=4, ensure_ascii=False)
+        with open(filename, 'w', encoding="UTF-8") as jsonfile:
+            json.dump([v.dict for v in vacancies], jsonfile, indent=4, ensure_ascii=False)
 
 
-class HHAPIExporter:
-    pass
+def run_parser_hh():
+    parser_hh = HHAPIParser()
+    vacancies = parser_hh.fetch_vacancies()
+    parser_hh.export_vacancies(vacancies)
+    return vacancies
 
 
+def run_parser_Superjob():
+    parser_Superjob = SuperjobAPIParser()
+    vacancies = parser_Superjob.fetch_vacancies()
+    parser_Superjob.export_vacancies(vacancies)
+    return vacancies
 
-# superjob_api_parser.py
-class SuperjobAPIParser:
+
+class SuperjobAPIParser(VacancyParser):
     def __init__(self):
-        # Получаем токен авторизации приложения
         self.token = self.get_authorization_token()
 
     def get_authorization_token(self):
         # Задаем параметры запроса
-        params = {'login': 'mc_gra_dy@mail.ru', 'password': '240615Nastya', 'client_id': '2270', 'client_secret': 'v3.r.137463232.a085bb1aaa3c51a9b7b88481b7075aa785b24619.5bc49efdee8512297bdf804e2d7b7cc7f3736ec3', 'scope': 'api'}
+        params = {
+            'login': 'mc_gra_dy@mail.ru',
+            'password': '240615Nastya',
+            'client_id': '2270',
+            'client_secret': 'v3.r.137463232.a085bb1aaa3c51a9b7b88481b7075aa785b24619.5bc49efdee8512297bdf804e2d7b7cc7f3736ec3',
+            'scope': 'api'
+        }
 
         # Отправляем POST-запрос на получение токена авторизации
         auth_url = 'https://api.superjob.ru/2.0/oauth2/password/'
@@ -66,9 +113,11 @@ class SuperjobAPIParser:
         # Возвращаем токен авторизации
         return response.json()['access_token']
 
-    def fetch_vacancies_Superjob(self, salary_from = '60000'):
+    def fetch_vacancies(self, salary_from=None):
+
         # Задаем параметры запроса
-        headers = {'X-Api-App-Id': 'v3.r.137463232.a085bb1aaa3c51a9b7b88481b7075aa785b24619.5bc49efdee8512297bdf804e2d7b7cc7f3736ec3'}
+        headers = {
+            'X-Api-App-Id': 'v3.r.137463232.a085bb1aaa3c51a9b7b88481b7075aa785b24619.5bc49efdee8512297bdf804e2d7b7cc7f3736ec3'}
         params = {'keywords': 'Python разработчик', 'count': '100'}
 
         # Отправляем GET-запрос на получение списка вакансий
@@ -87,40 +136,17 @@ class SuperjobAPIParser:
         vacancies = []
         for vacancy_data in response.json()['objects']:
             if vacancy_data['payment_from'] is not None:
-                vacancy = Vacancy(employer_name=vacancy_data['profession'], name=vacancy_data['firm_name'], salary=vacancy_data['payment_from'], alternate_url=vacancy_data['link'])
+                vacancy = Vacancy(
+                    employer_name=vacancy_data['profession'],
+                    name=vacancy_data['firm_name'],
+                    salary=vacancy_data['payment_from'],
+                    alternate_url=vacancy_data['link']
+                )
                 vacancies.append(vacancy)
         # Возвращаем список вакансий
         return vacancies
 
-
-class SuperjobAPIExporter:
-    pass
-
-def run_parser_hh():
-    # Создаем экземпляр парсера
-    parser_hh = HHAPIParser()
-
-    # Получаем вакансии через API
-    vacancies_hh = parser_hh.fetch_vacancies_hh()
-    print(vacancies_hh)
-
-    # Создаем экземпляр экспортера
-    exporter_hh = HHAPIExporter()
-    print(exporter_hh)
-    # Экспортируем данные в нужном формате
-    parser_hh.export_vacancies_hh(vacancies_hh)
-
-def run_parser_Superjob():
-    # Создаем экземпляр парсера
-    parser_Superjob = SuperjobAPIParser()
-    parser_hh = HHAPIParser()
-
-    # Получаем вакансии через API
-    vacancies_Superjob = parser_Superjob.fetch_vacancies_Superjob()
-    print(vacancies_Superjob)
-
-    # Создаем экземпляр экспортера
-    exporter_Superjob = HHAPIExporter()
-    print(exporter_Superjob)
-    # Экспортируем данные в нужном формате
-    parser_hh.export_vacancies_hh(vacancies_Superjob)
+    def export_vacancies(self, vacancies, filename='vacancies_superjob.json'):
+        # Экспортируем вакансии в json-файл
+        with open(filename, 'w', encoding="UTF-8") as jsonfile:
+            json.dump([v.dict for v in vacancies], jsonfile, indent=4, ensure_ascii=False)
